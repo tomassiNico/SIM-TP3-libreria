e37f6e523767f104f6a769b7121b8d8c669b9dcb
tomassiNico <xtomassix@gmail.com>
2018-04-22 15:43:02 -0300

    lala

--- src/distribuciones/Exponencial.java
+++ src/distribuciones/Exponencial.java
@@ -5,10 +5,39 @@
  */
 package distribuciones;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.logging.Level;
+
 /**
  *
  * @author nicolastomassi
  */
 public class Exponencial {
+    
+    private float media;
+    private float lambda;
+    
+    public Exponencial(){
+        
+    }
+    
+    public Exponencial(float media) {
+        this.media = media;
+        this.lambda = 1/this.media;
+    }
+    
+//    public ArrayList generarNumeros(int cantidad) {
+//        ArrayList numeros = new ArrayList();
+//        Random r = new Random();
+//        double numeroGenerado;
+//        
+//        for (int i = 0; i < cantidad; i++) {
+//            numeroGenerado = (-1/this.lambda) * Math.log((1-r.nextFloat()));
+//        }
+        
+    
+    
+    
     
 }
